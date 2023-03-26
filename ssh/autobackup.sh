320f943611c4499786135d8a9fe4a097a3e9834c
casper9 <50382728+casper9@users.noreply.github.com>
2023-03-26 22:11:17 +0800

    Update autobackup.sh

--- ssh/autobackup.sh
+++ ssh/autobackup.sh
@@ -32,7 +32,6 @@ echo -e "\e[31mPermission Denied!\e[0m";
 exit 0
 fi
 clear
-accouclear
 IP=$(wget -qO- ipinfo.io/ip);
 date=$(date +"%Y-%m-%d");
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
