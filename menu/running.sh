4ef20d75ab2394e1a0ea07aa66d3a424e89bd617
casper9 <50382728+casper9@users.noreply.github.com>
2023-03-27 00:12:28 +0800

    Update running.sh

--- menu/running.sh
+++ menu/running.sh
@@ -392,3 +392,5 @@ echo -e "$COLOR1 ${NC}                ${WH}♤ PREMIUM ♤ ${NC}                
 echo -e "$COLOR1 ${NC}                ${WH}◇   BY   ◇ ${NC}                    $COLOR1 $NC"
 echo -e "$COLOR1 ${NC}             ${WH}♡ C A S P E R ♡ ${NC}                 $COLOR1 $NC"
 echo -e "$COLOR1└───────────────────────────────────────────────────┘${NC}"
+read -n 1 -s -r -p "Press any key to back on menu"
+menu
