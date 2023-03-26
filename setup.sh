646d62b68415615180d4f3ee35e88ebe19b76404
casper9 <50382728+casper9@users.noreply.github.com>
2023-03-26 10:48:06 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -279,7 +279,7 @@ if [ ! -f "/etc/log-create-user.log" ]; then
 echo "Log All Account " > /etc/log-create-user.log
 fi
 history -c
-serverV=$( curl -sS https://raw.githubusercontent.com/casper9/Profile/main/Profile/permission/versi  )
+serverV=$( curl -sS https://raw.githubusercontent.com/permission/main/versi  )
 echo $serverV > /opt/.ver
 aureb=$(cat /home/re_otm)
 b=11
