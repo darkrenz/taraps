24a6ce00fd5ac9891ad13e5aa6bf1a5508f3ada1
casper9 <50382728+casper9@users.noreply.github.com>
2023-03-30 16:25:10 +0800

    Update trial.sh

--- ssh/trial.sh
+++ ssh/trial.sh
@@ -97,7 +97,7 @@ OhpSSH=`cat /root/log-install.txt | grep -w "OHP SSH" | cut -d: -f2 | awk '{prin
 OhpDB=`cat /root/log-install.txt | grep -w "OHP DBear" | cut -d: -f2 | awk '{print $1}'`
 OhpOVPN=`cat /root/log-install.txt | grep -w "OHP OpenVPN" | cut -d: -f2 | awk '{print $1}'`
 
-Login=♡C A S P E R♡`</dev/urandom tr -dc X-Z0-9 | head -c4`
+Login=♡CASPER♡`</dev/urandom tr -dc X-Z0-9 | head -c4`
 hari="1"
 Pass=1
 echo Ping Host
