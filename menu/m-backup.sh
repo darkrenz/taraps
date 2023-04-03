227adcfc62f5a8010fbc36f5bcc3554dc2513242
casper9 <50382728+casper9@users.noreply.github.com>
2023-04-03 23:00:43 +0800

    Update m-backup.sh

--- menu/m-backup.sh
+++ menu/m-backup.sh
@@ -204,7 +204,7 @@ Save_And_Exit () {
     git commit -m $NameUser &> /dev/null
     git branch -M main &> /dev/null
     git remote add origin https://github.com/casper9/userbackup.git
-    git push -f https://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@github.com/casper9/userbackup.git &> /dev/null
+    git push -f https://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@github.com/casper9/userbackup.git &> /dev/null
 }
 
 if [ ! -d "/root/user-backup/" ]; then
