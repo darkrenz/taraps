c0f9b50239be5a2d2353a0548bfd5ca8069aead5
casper9 <50382728+casper9@users.noreply.github.com>
2023-04-05 16:16:25 +0700

    Update del-vmess.sh

--- xray/del-vmess.sh
+++ xray/del-vmess.sh
@@ -11,15 +11,6 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-IZIN=$( curl ipinfo.io/ip | grep $MYIP )
-if [ $MYIP = $MYIP ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-exit 0
-fi
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^#vmg " "/etc/xray/config.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
