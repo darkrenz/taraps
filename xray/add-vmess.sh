2b8eaf7466e367141c70560839a25cd582762812
casper9 <50382728+casper9@users.noreply.github.com>
2023-04-05 11:16:48 +0700

    Update add-vmess.sh

--- xray/add-vmess.sh
+++ xray/add-vmess.sh
@@ -116,6 +116,8 @@ v2ray-menu
 uuid=$(cat /proc/sys/kernel/random/uuid)
 read -p "Expired (days): " masaaktif
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+sed -i '/#vmess$/a\#vm '"$user $exp"'\
+},vm{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/config.json
 sed -i '/#vmess$/a\### '"$user $exp"'\
 },{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/config.json
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
