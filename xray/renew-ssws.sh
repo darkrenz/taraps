64f38be9fe260c1300e096b2bb29404d13d8be5b
casper9 <50382728+casper9@users.noreply.github.com>
2023-04-05 11:42:46 +0800

    Update renew-ssws.sh

--- xray/renew-ssws.sh
+++ xray/renew-ssws.sh
@@ -35,7 +35,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
 		echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
         echo ""
         read -n 1 -s -r -p "Press any key to back on menu"
-        menu
+        m-sshws
 	fi
 
 	clear
